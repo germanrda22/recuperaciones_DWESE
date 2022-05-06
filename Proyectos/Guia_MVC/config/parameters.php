f2ccd305f6a828daae1cdbf0c5f867d8983bb02b
Germán Ruiz del Arco <germanrdarco@pc141-10.18700293.18.andared.ced.junta-andalucia.es>
2022-05-06 08:43:06 +0200

    Mejora controlador frontal

---
+++ Proyectos/Guia_MVC/config/parameters.php
@@ -0,0 +1,10 @@
+<?php
+
+// Constante con el dominio de mi proyecto
+define("base_url", "https://localhost/recuperacion_DWESE/Proyectos/Guia_MVC");
+// Defino el controlador por defecto de mi proyecto
+define("controller_default", "Controller\ContactoController");
+define('action_default', "mostrarTodos");
+
+// Estas constantes se usarán cuando hagamos llamadas a imágenes en las vistas
+// o a ULRs del proyecto en las vistas
--- Proyectos/Guia_MVC/controllers/FrontController.php
+++ Proyectos/Guia_MVC/controllers/FrontController.php
@@ -5,28 +5,49 @@ namespace Controllers;
 class FrontController{
     public static function main()
     {
+        function show_error()
+        {
+            $error = new ErrorController(); // Usamos este controlador para mostrar los errores
+            $error->index();
+        }
         if (!empty($_GET))
         {
             if (isset($_GET['controller'])) 
-            {
+            { 
                 // Al llamarse todos igual acortamos la URL añadiendo directamente nosotrosController 
                 $nombre_controlador = 'Controllers\\'.$_GET['controller'].'Controller';
-                if(class_exists($nombre_controlador))
+            
+
+            }elseif(!isset($_GET['controller']) and !isset($_GET['action'])){
+                    $nombre_controlador = controller_default; // Se establece un archivo de parámetros
+            }else
+            {
+                show_error();
+                exit();
+            }
+                
+            // Si todo va bien creamos una instancia del controlador y llamamos a la acción
+                
+            if(class_exists($nombre_controlador))
+            {
+                $controlador = new $nombre_controlador();
+                if(isset($_GET['action']) and method_exists($controlador, $_GET['action']))
                 {
-                    $controlador = new $nombre_controlador();
-                    if(isset($_GET['action']) and method_exists($controlador, $_GET['action']))
-                    {
-                        $action = $_GET['action'];
-                        $controlador->$action();
-                    }else
-                    {
-                        echo 'La página que buscas no existe';
-                    }
+                    $action = $_GET['action'];
+                    $controlador->$action();
+                }elseif(!isset($_GET['controller']) and !isset($_GET['action']))
+                {
+                    $action_default = action_default;
+                    $controlador->$action_default();
                 }else
                 {
-                    echo 'La página que buscas no existe';
+                    show_error();
                 }
+            }else
+            {
+                show_error();
             }
+            
         }else
         {
             echo "<a href='http://localhost/recuperacion_DWESE/Proyectos/Guia_MVC/?controller=Contacto&action=mostrarTodos'>Mostrar Todos</a><br>";
